b1921c1d102014ee4cba55714204ac438166a0b2
secw01f <16842677+secw01f@users.noreply.github.com>
2022-04-06 16:25:22 -0400

    added table row functionality

--- lib/template.py
+++ lib/template.py
@@ -30,6 +30,14 @@ def template_config(input_file, config_file):
                 elif config['elements'][x]['type'] == 'td':
                     element.clear()
                     element.append(config['elements'][x]['text'])
+                elif config['elements'][x]['type'] == 'table':
+                    for row in range(0, len(config['elements'][x]['rows'])):
+                        add_row = html.new_tag('tr')
+                        element.append(add_row)
+                        for y in range(0, len(config['elements'][x]['rows'][row]['td'])):
+                            add_data = html.new_tag('td', style=config['elements'][x]['rows'][row]['td'][y]['css'])
+                            add_row.append(add_data)
+                            add_data.string = config['elements'][x]['rows'][row]['td'][y]['text']
 
         with open(input_file, 'w') as output:
             output.write(str(html))
